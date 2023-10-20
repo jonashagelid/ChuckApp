a4c386215e4d1ffa602203845f34de41e60a3dd8
JH <jonas.hagelid@gmail.com>
2023-10-20 02:47:11 +0200

    Added category selection and expended api service

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,13 +10,18 @@ android {
     defaultConfig {
         applicationId = "com.example.chuckapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
+        }
+        javaCompileOptions {
+            annotationProcessorOptions {
+                argument("jvmTarget", "1.8")
+            }
         }
     }
 
@@ -51,7 +56,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -69,3 +74,4 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
+
